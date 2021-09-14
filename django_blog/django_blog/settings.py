c6cb3de48c31a2823bb173bb7838bf04dac1471a
William Phan <hoangviet191@gmail.com>
2021-09-14 05:48:16 +0000

    [Django Blog] Update settings for PRD

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bz)iead8t%1bugzd@bts(b^y_6mqk1nxu=%v#h!rh0xv8h_h5!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['williamphan.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
